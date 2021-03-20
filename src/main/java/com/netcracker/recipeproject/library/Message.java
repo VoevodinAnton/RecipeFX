714d3c08099131c965066df4931202521f9c327d
Galina Vazyukova <gvazyukova@mail.ru>
2021-03-20 20:50:03 +0400

    Предметная область

--- src/main/java/com/netcracker/recipeproject/library/Message.java
+++ src/main/java/com/netcracker/recipeproject/library/Message.java
@@ -1,6 +1,28 @@
 package com.netcracker.recipeproject.library;
 
 public class Message {
-    int flag;
-    Object obj;
+    private int flag;
+    private Object obj;
+
+    public Message(int flag, Object obj){
+        this.flag = flag;
+        this. obj = obj;
+    }
+
+    public int getFlag() {
+        return flag;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
 }
+
