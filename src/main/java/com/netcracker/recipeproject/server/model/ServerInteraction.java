036557b7fa539e9f8e7e2e84b88c9b4008f3e77c
Anton <voevodin_anton_2000@mail.ru>
2021-03-20 23:26:30 +0400

    trying to connect to the client

--- src/main/java/com/netcracker/recipeproject/server/model/ServerInteraction.java
+++ src/main/java/com/netcracker/recipeproject/server/model/ServerInteraction.java
@@ -1,28 +1,61 @@
 package com.netcracker.recipeproject.server.model;
 
-import com.netcracker.recipeproject.library.Interaction;
 import com.netcracker.recipeproject.library.Message;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ServerInteraction implements Interaction {
+public class ServerInteraction{
+    int port;
 
-    @Override
+    public ServerInteraction(int port){
+        this.port = port;
+    }
+
     public Message getMessage() {
         return null;
     }
 
-    @Override
     public void messageRequest(Message message, Socket socket) {
 
     }
 
-    @Override
-    public void process() {
+
+    public void messageRequest(Socket socket) {
+        ObjectOutputStream objOut = null;
+        ObjectInputStream objIn = null;
+
+
 
     }
 
-    @Override
+    public void process() {
+        try {
+
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Сервер запущен");
+            Socket socket;
+            while (true) {
+                try {
+                    socket = serverSocket.accept();
+                    System.out.println(
+                            "Установлено соединение с клиентом");
+                    messageRequest(socket);
+                } catch (IOException ex) {
+                    System.err.println(
+                            "Ошибка при установлении связи");
+                }
+            }
+        } catch (IOException ex) {
+            System.err.println("Невозможно открыть порт");
+        }
+
+
+    }
+
     public void doCommand(Message message) {
 
     }
