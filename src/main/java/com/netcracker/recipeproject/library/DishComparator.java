e6aba1a3edbc7132b2d48a044551a445f19495b5
Galina Vazyukova <gvazyukova@mail.ru>
2021-04-20 14:42:08 +0400

    made last functions

--- src/main/java/com/netcracker/recipeproject/library/DishComparator.java
+++ src/main/java/com/netcracker/recipeproject/library/DishComparator.java
@@ -7,8 +7,19 @@ public class DishComparator implements Comparator<Dish> {
     public int compare(Dish dish1, Dish dish2) {
         String[] parts1 = dish1.getCookingTime().split(" ");
         Integer thisTime = Integer.parseInt(parts1[0]);
+        String thisTimeUnit = parts1[1];
         String[] parts2 = dish2.getCookingTime().split(" ");
         Integer anotherTime = Integer.parseInt(parts2[0]);
-        return thisTime.compareTo(anotherTime);
+        String anotherTimeUnit = parts2[1];
+        int result = 0;
+        if(thisTimeUnit.contains("час") && anotherTimeUnit.contains("час"))
+            result = 0;
+        else if (thisTimeUnit.contains("час") && !anotherTimeUnit.contains("час"))
+            result = 2;
+        else if (!thisTimeUnit.contains("час") && anotherTimeUnit.contains("час"))
+            result = -2;
+        return thisTime.compareTo(anotherTime) + result;
     }
+
+
 }
